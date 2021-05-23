4478defdb4b276114248d3ed2d4648c44720093a
kdsbest <kdsbest@gmx.de>
2021-05-23 15:38:14 +0200

    Cosmos DB Secrets via Env

--- MMORPGServer/CommonServer/Configuration/CosmosDbConfiguration.cs
+++ MMORPGServer/CommonServer/Configuration/CosmosDbConfiguration.cs
@@ -1,13 +1,15 @@
-﻿namespace CommonServer.Configuration
+﻿using System;
+
+namespace CommonServer.Configuration
 {
     public static class CosmosDbConfiguration
     {
 #if DEBUG
-        public static readonly string CosmosDbEndpointUrl = "https://localhost:8081";
+        public static readonly string CosmosDbEndpointUrl = Environment.GetEnvironmentVariable("CosmosUrl") ?? "https://localhost:8081";
 #else
-        public static readonly string CosmosDbEndpointUrl = "https://host.docker.internal:8081";
+        public static readonly string CosmosDbEndpointUrl = Environment.GetEnvironmentVariable("CosmosUrl") ?? "https://host.docker.internal:8081";
 #endif
-        public static readonly string CosmosDbKey = "C2y6yDjf5/R+ob0N8A7Cgv30VRDJIWEHLM+4QDU5DE2nQ9nDuVTqobD4b8mGGyPMbIZnqyMsEcaGQy67XIw/Jw==";
+        public static readonly string CosmosDbKey = Environment.GetEnvironmentVariable("CosmosKey") ?? "C2y6yDjf5/R+ob0N8A7Cgv30VRDJIWEHLM+4QDU5DE2nQ9nDuVTqobD4b8mGGyPMbIZnqyMsEcaGQy67XIw/Jw==";
 
         public static readonly string CosmosDb = "MMORPGDb";
         public static readonly string CosmosDbUserDbCollection = "UserCollection";
