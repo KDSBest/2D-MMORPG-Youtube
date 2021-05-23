0de80b7b011dbfc9a2e201fea06f62ab8d083df9
kdsbest <kdsbest@gmx.de>
2021-05-23 15:10:39 +0200

    enable admin acr

--- terraform/mmorpg.tf
+++ terraform/mmorpg.tf
@@ -78,5 +78,5 @@ resource "azurerm_container_registry" "acr" {
   resource_group_name      = azurerm_resource_group.k8s.name
   location                 = azurerm_resource_group.k8s.location
   sku                      = "Standard"
-  admin_enabled            = false
+  admin_enabled            = true
 }
