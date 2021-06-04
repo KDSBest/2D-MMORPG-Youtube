caed2dc167a3955657564f59f5eca8b3a6915a8a
kdsbest <kdsbest@gmx.de>
2021-06-04 11:26:28 +0200

    dns prefix

--- terraform/mmorpg.tf
+++ terraform/mmorpg.tf
@@ -4,10 +4,11 @@ resource "azurerm_resource_group" "k8s" {
 }
 
 resource "azurerm_kubernetes_cluster" "k8s" {
-    name                = var.cluster_name
-    location            = azurerm_resource_group.k8s.location
-    resource_group_name = azurerm_resource_group.k8s.name
-    dns_prefix          = var.dns_prefix
+    name                       = var.cluster_name
+    location                   = azurerm_resource_group.k8s.location
+    resource_group_name        = azurerm_resource_group.k8s.name
+    dns_prefix                 = var.dns_prefix
+    dns_prefix_private_cluster = var.dns_prefix
 
     linux_profile {
         admin_username = "k8sadmin"
