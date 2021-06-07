4cbde3ae18c85b5645f992e5fdf79bff413658fa
kdsbest <kdsbest@gmx.de>
2021-06-08 00:41:03 +0200

    nginx

--- terraform/mmorpg.tf
+++ terraform/mmorpg.tf
@@ -8,7 +8,6 @@ resource "azurerm_kubernetes_cluster" "k8s" {
     location                   = azurerm_resource_group.k8s.location
     resource_group_name        = azurerm_resource_group.k8s.name
     dns_prefix                 = var.dns_prefix
-    dns_prefix_private_cluster = var.dns_prefix
 
     linux_profile {
         admin_username = "k8sadmin"
