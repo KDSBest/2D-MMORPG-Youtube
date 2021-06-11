8c837f96729de03fd43954cb58a26c9b9836e69e
kdsbest <kdsbest@gmx.de>
2021-06-11 18:31:33 +0200

    Jwt secret configurable by pipeline

--- MMORPGServer/CommonServer/Configuration/SecurityConfiguration.cs
+++ MMORPGServer/CommonServer/Configuration/SecurityConfiguration.cs
@@ -1,9 +1,11 @@
-﻿namespace CommonServer.Configuration
+﻿using System;
+
+namespace CommonServer.Configuration
 {
     public static class SecurityConfiguration
     {
         // TODO Read from env variable for k8s
-        public static readonly string JwtSecret = "HelloWorld!HelloWorld!HelloWorld!HelloWorld!";
+        public static readonly string JwtSecret = Environment.GetEnvironmentVariable("JwtSecret") ?? "HelloWorld!HelloWorld!HelloWorld!HelloWorld!";
 
         public static readonly string EmailClaimType = "email";
         public static readonly string CharClaimType = "char";
